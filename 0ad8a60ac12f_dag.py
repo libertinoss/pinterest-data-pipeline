5a3ca375d91a25c4a7c87100ab2b82d0971a4c86
Thomas Gyongy <thomas.gyongy@gmail.com>
2024-01-15 12:33:20 +0000

    user posting emulation scripts finalised

---
+++ 0ad8a60ac12f_dag.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+#Define params for Submit Run Operator
+notebook_task = {
+    'notebook_path': '/Users/thomas.gyongy@gmail.com/data_cleaning_notebook',
+}
+
+
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+
+default_args = {
+    'owner': 'Thomas_G',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG('0ad8a60ac12f_dag',
+    # should be a datetime format
+    start_date=datetime(2024, 1, 13, 0, 0, 0),
+    # check out possible intervals, should be a string
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
